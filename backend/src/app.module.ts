c156727d6315d98363b05e94724d7d84f665f2ad
Malik <malikhussein229@gmail.com>
2025-08-29 16:00:43 +0300

    feat: integrate JWT authentication by adding Passport and Jwt modules; update JwtStrategy to use ConfigService for secret management

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { UserModule } from './modules/user/user.module';
 import { DepartmentModule } from './modules/department/department.module';
 import { EmployeeModule } from './modules/employee/employee.module';
@@ -13,6 +15,18 @@ import { AppDataSource } from './common/config/typeorm.config';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(AppDataSource.options),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      global: true, // This makes JWT module available globally
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET') || 'default_secret',
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '1d',
+        },
+      }),
+      inject: [ConfigService],
+    }),
     UserModule,
     DepartmentModule,
     EmployeeModule,
